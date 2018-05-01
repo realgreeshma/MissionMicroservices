faecf76048876119c994235337d6d28868d46b98
developer <developer>
2018-05-01 11:46:43 -0500

    My journal logs

---
+++ src/main/java/com/journal/CaseChangeString.java
@@ -0,0 +1,9 @@
+package com.journal;
+
+public class CaseChangeString {
+    public static void main(String[] args) {
+        String str = new String("STrength");
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+    }
+}
---
+++ src/main/java/com/journal/CharReplace.java
@@ -0,0 +1,9 @@
+package com.journal;
+
+public class CharReplace {
+    public static void main(String[] args) {
+        String str = new String("Greeshma");
+        System.out.println(str.replaceAll(Character.toString('e'),""));
+
+    }
+}
